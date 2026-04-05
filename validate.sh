022bdfb777e67358485c86a244d10a750ad1ff3e
Fredrik Førde Lindhagen <fredrik@lindhagen.io>
2026-04-05 12:27:40 +0200

    Use json5 i .json ending

--- validate.sh
+++ validate.sh
@@ -8,14 +8,9 @@ echo "Now validating.."
 echo "Note: The logs for 'keywords ignored' can be ignored."
 
 for x in $(ls -1 *.json *.json5 2>/dev/null | grep -v "^package.json$" | grep -v "^package-lock.json$" | grep -v "^renovate-schema.json$"); do
-  if [[ "$x" == *.json5 ]]; then
-    echo "Validating $x (converting from JSON5)"
-    tmpfile=$(mktemp)
-    npx json5 -c "$x" 2>/dev/null
-    npx ajv --strict=false -s renovate-schema.json -d "${x%.json5}.json"
-    rm -f "${x%.json5}.json"
-  else
-    echo "Validating $x"
-    npx ajv --strict=false -s renovate-schema.json -d "$x"
-  fi
+  echo "Validating $x"
+  tmpfile=$(mktemp).json
+  npx json5 -o "$tmpfile" "$x"
+  npx ajv --strict=false -s renovate-schema.json -d "$tmpfile"
+  rm -f "$tmpfile"
 done
